b0cc35f8b33e7e86b856c2f85a8dd55aa42cc5b7
Anna Jang <anna.jangg@gmail.com>
2021-12-02 01:16:21 -0500

    hack110 project complete!

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
-from helpers import assign_name, user
+from helpers import find_animal, user
+
+# run the website by typing 'python -m app'
 
 app: Flask = Flask(__name__)
 users: list[user] = []
@@ -9,38 +11,33 @@ user_number: int = 0
 def index():
     return render_template('index.html')
 
-@app.route('/todo', methods=["GET", "POST"])
-def todo():
-    global users
-    global user_number
-    
-    return render_template("todo.html")
-
-
-
-
-
-@app.route('/add_assign', methods=["GET", "POST"])
-def add_assign():
+@app.route('/quiz', methods=["GET", "POST"])
+def quiz():
     if request.method == "POST":
         global users
         global user_number
 
-        assignment: str = request.form['assignment']
+        fname: str = request.form ['fname']
+        lname: str = request.form['lname']
+        personality: str = request.form['personality']
+        realm: str = request.form['realm']
+        color: str = request.form['color']
+        music: str = request.form['music']
+        power: str = request.form['power']
+        princess: str = request.form['princess']
+        holiday: str = request.form['holiday']
 
-        # assign_name: str = request.form['assignment']
+        if fname == '' or lname == '' or personality == '' or realm == '' or color == '' or music == '' or power == '' or princess == '' or holiday == '':
+            return render_template("quiz.html")
 
-        if assign_name == '':
-            return render_template("add_assign.html")
-
-        name: str = assign_name(assignment)
-        new_user: user = user(user_number, assignment, name)
+        animal: str = find_animal(music)
+        new_user: user = user(user_number, fname, lname, animal)
         users.append(new_user)
 
         user_number += 1
 
-        return render_template("todo.html", name=name)
-    return render_template("add_assign.html")
+        return render_template("result.html", animal=animal)
+    return render_template("quiz.html")
 
 @app.route('/all-results')
 def all_results():
@@ -49,7 +46,6 @@ def all_results():
 @app.route('/user<usernumber>')
 def display_user(usernumber: str):
     return render_template('user.html', user=users[int(usernumber)])
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- helpers.py
+++ helpers.py
@@ -1,10 +1,21 @@
-def assign_name(name: str) -> str:
-    return name
+def find_animal(music: str) -> str:
+    if music == 'jazz':
+        return 'Morris'
+    elif music == 'rap':
+        return 'Abu'
+    elif music == 'folk':
+        return 'Sven'
+    else:
+        return 'Fire Spirit'
 
 class user:
     id: int
-    my_assignment: str
+    first_name: str
+    last_name: str
+    animal: str
 
-    def __init__(self, id: int, assignment: str, name: str):
+    def __init__(self, id: int, fname: str, lname: str, animal: str):
         self.id = id
-        self.my_assignment = assignment
+        self.first_name = fname
+        self.last_name = lname
+        self.animal = animal
